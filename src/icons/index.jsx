b1f3cf4158b4f63bb5b6b7eaed306481ddd14ffc
André Lusegardis <lusegardis@gmail.com>
2022-09-27 10:42:24 -0300

    removed comment

--- src/icons/index.jsx
+++ src/icons/index.jsx
@@ -1,11 +1,6 @@
 export const Icon = ({ children, height, width, viewBox, color }) => {
   return (
     <>
-      {/* <svg
-        height={`${size}px`}
-        width={`${size}px`}
-        viewBox={`0 0 ${size} ${size}`}
-      > */}
       <svg height={height} width={width} viewBox={viewBox} color={color}>
         {children}
       </svg>
